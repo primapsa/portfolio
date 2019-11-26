7834abf9800413c589f4c6be465ea31cdfbf120c
Siarhei Prymakovich <prima.psa@gmail.com>
2019-11-27 01:54:01 +0300

    feat: add projects

---
+++ repair-design-project/script.js
@@ -0,0 +1,14 @@
+const resolution = document.querySelector('.resolution');
+const body = document.querySelector('body');
+resolution.addEventListener('click', ()=>{
+    const bodyClass = body.classList[0];
+    let rename ='';    
+    if(bodyClass === 'button-mobile'){
+        body.classList.remove('button-mobile');
+        rename = 'Mobile';
+    }else {
+        body.classList.add('button-mobile');
+        rename = 'Desktop';
+    }   
+    resolution.innerHTML = rename;
+});
