74aa746852c204f38e5ae834c0d5d607b6b4e19d
Siarhei Prymakovich <prima.psa@gmail.com>
2019-11-27 01:56:23 +0300

    feat: add add slider and project buttons

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.querySelectorAll(".slider__item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add('hide');
+        slides[i].classList.remove('show');
+    }  
+    slides[slideIndex - 1].classList.remove('hide'); 
+    slides[slideIndex - 1].classList.add('show');   
+}
 const button = document.querySelector('.panel__control');
 const panel = document.querySelector('.panel__content');
+
+const prev = document.querySelector('.slider__prev');
+const next = document.querySelector('.slider__next');
+
+prev.addEventListener('click',minusSlide);
+next.addEventListener('click',plusSlide);
 
 button.addEventListener('click',()=>{
     
@@ -11,4 +49,4 @@ button.addEventListener('click',()=>{
         panel.classList.remove('hide'); 
         panel.classList.add('show');
     }
-})
+});
